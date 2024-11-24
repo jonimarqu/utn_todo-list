ab963ca7905caf839c444474ebadaa643629914c
diegui98 <diegui98@live.com.ar>
2024-11-24 17:48:37 -0300

    Revert "metodo tiempoDedicadoPorTarea + atributo fechaCompletado en Tarea"
    This reverts commit c1d045cd13fbde48fb6bd3d33d9d4be686c01151.

--- src/clases/tarea.ts
+++ src/clases/tarea.ts
@@ -2,85 +2,78 @@ import Categoria from "./categoria";
 import { Estado } from "../enums/Estado";
 import { Prioridad } from "../enums/Prioridad";
 export default class Tarea {
-  private avance: number = 0;
-  private estado: Estado = Estado.PENDIENTE;
-  private etiquetas: string[] = [];
-  private activo: boolean = true;
-  private fechaCompletado: Date | undefined;
+    private avance: number = 0;
+    private estado: Estado = Estado.PENDIENTE;
+    private etiquetas: string[] = [];
+    private activo: boolean = true;
 
-  constructor(
-    private id: number,
-    private titulo: string,
-    private descripcion: string,
-    private fechaCreacion: Date,
-    private fechaVencimiento: Date,
-    private prioridad: Prioridad,
-    private categoria: Categoria
-  ) {}
+    constructor(
+        private id: number,
+        private titulo: string,
+        private descripcion: string,
+        private fechaCreacion: Date,
+        private fechaVencimiento: Date,
+        private prioridad: Prioridad,
+        private categoria: Categoria
+    ) {}
 
-  public getId(): number {
-    return this.id;
-  }
-  public getTitulo(): string {
-    return this.titulo;
-  }
-  public getDescripcion(): string {
-    return this.descripcion;
-  }
-  public getfechaCreacion(): Date {
-    return this.fechaCreacion;
-  }
-  public getFechaVencimiento(): Date {
-    return this.fechaVencimiento;
-  }
-  public getFechaCompletado(): Date | undefined {
-    return this.fechaCompletado;
-  }
-  public getPrioridad(): Prioridad {
-    return this.prioridad;
-  }
-  public getCategoria(): Categoria {
-    return this.categoria;
-  }
-  public getEstado(): Estado {
-    return this.estado;
-  }
-  public getEtiquetas(): string[] {
-    return this.etiquetas;
-  }
-
-  actualizarAvance(nuevoAvance: number): void {
-    if ([0, 25, 50, 75, 100].includes(nuevoAvance)) {
-      this.avance = nuevoAvance;
-    } else {
-      console.error(
-        "Avance inválido. Los valores permitidos son 0, 25, 50, 75, 100."
-      );
+    public getId():number{
+        return this.id;
     }
-  }
+    public getTitulo():string{
+        return this.titulo;
+    }
+    public getDescripcion():string{
+        return this.descripcion;
+    }
+    public getfechaCreacion():Date{
+        return this.fechaCreacion;
+    }
+    public getFechaVencimiento():Date{
+        return this.fechaVencimiento;
+    }
+    public getPrioridad():Prioridad{
+        return this.prioridad;
+    }
+    public getCategoria():Categoria{
+        return this.categoria;
+    }
+    public getEstado():Estado{
+        return this.estado;
+    }
+    public getEtiquetas():string[]{
+        return this.etiquetas;
+    }
 
-  marcarComoCompletada(): void {
-    this.estado = Estado.COMPLETADA;
-    this.fechaCompletado = new Date();
-  }
+    actualizarAvance(nuevoAvance: number): void {
+        if ([0, 25, 50, 75, 100].includes(nuevoAvance)) {
+            this.avance = nuevoAvance;
+        } else {
+            console.error("Avance inválido. Los valores permitidos son 0, 25, 50, 75, 100.");
+        }
+    }
 
-  public static fromJSON(json: string): Tarea {
-    const data = JSON.parse(json);
-    const tarea: Tarea = new Tarea(
-      data.id,
-      data.titulo,
-      data.descripcion,
-      new Date(data.fechaCreacion),
-      new Date(data.fechaVencimiento),
-      data.prioridad,
-      new Categoria(data.categoria.id, data.categoria.nombre)
-    );
-    tarea.avance = data.avance;
-    tarea.estado = data.estado;
-    tarea.etiquetas = data.etiquetas;
-    tarea.activo = data.activo;
-    return tarea;
-  }
+    marcarComoCompletada(): void {
+        this.estado = Estado.COMPLETADA;
+    }
+	
+	public static fromJSON(json: string): Tarea {
+		const data = JSON.parse(json);
+		const tarea: Tarea = new Tarea(
+			data.id,
+			data.titulo,
+			data.descripcion,
+			new Date(data.fechaCreacion),
+			new Date(data.fechaVencimiento),
+			data.prioridad,
+			new Categoria(data.categoria.id, data.categoria.nombre)
+		);
+		tarea.avance = data.avance;
+		tarea.estado = data.estado;
+		tarea.etiquetas = data.etiquetas;
+		tarea.activo = data.activo;
+		return tarea;
+	}
 }
 
 export { Tarea, Prioridad, Estado, Categoria };
