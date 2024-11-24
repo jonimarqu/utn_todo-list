372e31a785c51deaf146c8086f5c55076d96d3e8
Valen <vsosa@fi.uba.ar>
2024-11-23 22:21:33 -0300

    separo clases y agrego modificacion a enums

--- src/clases/tarea.ts
+++ src/clases/tarea.ts
@@ -1,36 +1,56 @@
-enum Prioridad {
-    ALTA = 'Alta',
-    MEDIA = 'Media',
-    BAJA = 'Baja'
+import Categoria from "./categoria";
+export enum Prioridad {
+    ALTA = 1,
+    MEDIA = 2,
+    BAJA = 3,
 }
 
-enum Estado {
+export enum Estado {
     PENDIENTE = 'Pendiente',
     COMPLETADA = 'Completada'
 }
 
-class Categoria {
-    constructor(
-        public id: string,
-        public nombre: string
-    ) {}
-}
-
-class Tarea {
-    public avance: number = 0;
-    public estado: Estado = Estado.PENDIENTE;
-    public etiquetas: string[] = [];
+export default class Tarea {
+    private avance: number = 0;
+    private estado: Estado = Estado.PENDIENTE;
+    private etiquetas: string[] = [];
 
     constructor(
-        public id: number,
-        public titulo: string,
-        public descripcion: string,
-        public fechaCreacion: Date,
-        public fechaVencimiento: Date,
-        public prioridad: Prioridad,
-        public categoria: Categoria
+        private id: number,
+        private titulo: string,
+        private descripcion: string,
+        private fechaCreacion: Date,
+        private fechaVencimiento: Date,
+        private prioridad: Prioridad,
+        private categoria: Categoria
     ) {}
-
+    public getId():number{
+        return this.id;
+    }
+    public getTitulo():string{
+        return this.titulo;
+    }
+    public getDescripcion():string{
+        return this.descripcion;
+    }
+    public getfechaCreacion():Date{
+        return this.fechaCreacion;
+    }
+    public getFechaVencimiento():Date{
+        return this.fechaVencimiento;
+    }
+    public getPrioridad():Prioridad{
+        return this.prioridad;
+    }
+    public getCategoria():Categoria{
+        return this.categoria;
+    }
+    public getEstado():Estado{
+        return this.estado;
+    }
+    public getEtiquetas():string[]{
+        return this.etiquetas;
+    }
     actualizarAvance(nuevoAvance: number): void {
         if ([0, 25, 50, 75, 100].includes(nuevoAvance)) {
             this.avance = nuevoAvance;
